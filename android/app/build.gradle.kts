303decf683c4c06a92de1db002b5967d4f8e05c3
The Amiri <theamiri@Thes-MacBook-Pro.local>
2025-08-06 17:45:24 +0100

    change package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.stocktool"
+    namespace = "com.amiri.stocktool"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.stocktool"
+        applicationId = "com.amiri.stocktool"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/com/amiri/stocktool/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.amiri.stocktool
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() 
